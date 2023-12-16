7e4e374761ed68d7b9ed11ee17b29029cd24f5a1
Elisson15 <148507784+Elisson15@users.noreply.github.com>
2023-12-16 00:59:39 -0300

    Update dino.js

--- dino.js
+++ dino.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const dino = document.getElementById("dino");
     const suelo = document.getElementById("suelo");
+    const juego = document.getElementById("juego");
 
     let saltando = false;
 
@@ -17,11 +18,52 @@ document.addEventListener("DOMContentLoaded", function () {
         const intervalo = setInterval(function () {
             if (altura >= 150) {
                 clearInterval(intervalo);
-                saltando = false;
+                descender();
             } else {
                 altura += 5;
                 dino.style.bottom = altura + "px";
             }
         }, 20);
     }
+
+    function descender() {
+        let altura = 150;
+        const intervalo = setInterval(function () {
+            if (altura <= 20) {
+                clearInterval(intervalo);
+                saltando = false;
+            } else {
+                altura -= 5;
+                dino.style.bottom = altura + "px";
+            }
+        }, 20);
+    }
+
+    function colision() {
+        const dinoRect = dino.getBoundingClientRect();
+        const obstaculo = document.querySelector(".obstaculo");
+        const obstaculoRect = obstaculo.getBoundingClientRect();
+
+        return !(
+            dinoRect.bottom < obstaculoRect.top ||
+            dinoRect.top > obstaculoRect.bottom ||
+            dinoRect.right < obstaculoRect.left ||
+            dinoRect.left > obstaculoRect.right
+        );
+    }
+
+    function gameOver() {
+        alert("¡Game Over!");
+        location.reload(); // Recargar la página para reiniciar el juego
+    }
+
+    function actualizarJuego() {
+        if (colision()) {
+            gameOver();
+        } else {
+            requestAnimationFrame(actualizarJuego);
+        }
+    }
+
+    actualizarJuego();
 });
